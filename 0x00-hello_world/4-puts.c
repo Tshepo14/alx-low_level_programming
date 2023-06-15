9cf61704b63e28b5adbe7d1efa4b02207af8535c
Tshepo <tmoloto989@gmail.com>
2023-06-15 13:43:36 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - entry point 
  *
- * return: Always 0 (indicating success)
+ * return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
