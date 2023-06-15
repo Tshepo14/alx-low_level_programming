e137a28ec8ca9140cd941d92d00db2ddbfa032cb
Tshepo <tmoloto989@gmail.com>
2023-06-15 14:19:09 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function 
+ * Description: a program that prints a line with puts function 
  *
  * return: Always 0 (success)
  */
