861e4cd0974d2efb42b8a41385c8164158e0f40e
Tshepo <tmoloto989@gmail.com>
2023-06-15 14:58:25 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - entry point 
+ * main - Entry point of the program
  *
- * return: Always 0 (success)
+ * Description: Prints the message using the puts function.
+ *
+ * Return: Always 0 (indicating success)
  */
+
 int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
