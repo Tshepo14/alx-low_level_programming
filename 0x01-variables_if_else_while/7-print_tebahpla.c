bfce58357c8873f597300b36bc4a54615a067f0c
Tshepo <tmoloto989@gmail.com>
2023-06-18 13:34:56 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,39 +3,16 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the lowercase alphabet in reverse,
- *              followed by a new line, using the putchar function.
+ * Description: Prints the lowercase alphabet in reverse.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('z');
-	putchar('y');
-	putchar('x');
-	putchar('w');
-	putchar('v');
-	putchar('u');
-	putchar('t');
-	putchar('s');
-	putchar('r');
-	putchar('q');
-	putchar('p');
-	putchar('o');
-	putchar('n');
-	putchar('m');
-	putchar('l');
-	putchar('k');
-	putchar('j');
-	putchar('i');
-	putchar('h');
-	putchar('g');
-	putchar('f');
-	putchar('e');
-	putchar('d');
-	putchar('c');
-	putchar('b');
-	putchar('a');
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
 	putchar('\n');
 
