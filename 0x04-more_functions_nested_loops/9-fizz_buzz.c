97783940486f043dde55809fd937e63591540774
Tshepo <tmoloto989@gmail.com>
2023-06-23 14:31:30 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
@@ -27,10 +27,13 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
+		if (i !=100)
+		{
+			printf(" ");
+		}
 	}
 
 	printf("\n");
-
 	return (0);
 }
 
