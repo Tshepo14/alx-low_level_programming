55d73212d8ab880e518d12d266d16b9c25165247
Tshepo <tmoloto989@gmail.com>
2023-06-23 16:17:17 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,29 +11,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz ");
+			printf(" Fizz");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 3 != 0)
 		{
-			printf("Fizz ");
+			printf(" Buzz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", i);
-		}
-		if (i != 100)
-		{
-			printf(" ");
+			printf(" %d", i);
 		}
 	}
-
 	printf("\n");
 	return (0);
 }
-
