c545a184516d897ed33abd29ad408e6cbdc283e6
Tshepo <tmoloto989@gmail.com>
2023-08-01 01:11:35 -0700

    updated header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,7 +10,19 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct listint_s
 {
