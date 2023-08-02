2ec97fc75d249496d5a049254a1e568391ea8489
Tshepo <tmoloto989@gmail.com>
2023-08-01 23:55:39 -0700

    a C program that prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,31 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints the message using printf or puts functions
- *
- * Return: Always 1 (indicating failure)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - "
-			"Dora Korpar, 2015-10-19\n";
-	ssize_t len = strlen(message);
-	ssize_t written;
-
-	written = write(STDERR_FILENO, message, len);
-	if (written != len)
-	{
-		int error = (written == -1) ? errno : EIO;
-		char *errorMsg = "Error writing to standard error\n";
-		ssize_t errorLen = strlen(errorMsg);
-
-		write(STDERR_FILENO, errorMsg, errorLen);
-		return (error);
-	}
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
